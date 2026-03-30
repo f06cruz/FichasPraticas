4ebf61098b16bd98af4db880c16e7db111b9a7dd
unknown <swd13@CESAEPRT.WAN>
2026-03-30 16:23:49 +0100

    Update Ex06_2.java

--- src/FichaPratica01/Ex06_2.java
+++ src/FichaPratica01/Ex06_2.java
@@ -3,7 +3,7 @@ package FichaPratica01;
 
 import java.util.Scanner;
 
-public class Ex06 {
+public class Ex06_2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
